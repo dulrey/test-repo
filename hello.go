45f1a3b2279f996cc1935bbabfad68364f11a60a
Danniel Ulrey <dulrey@Danniels-MacBook-Pro.local>
2020-07-09 19:54:28 -0400

    Add dockerfile, update hello and config.yml

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello Go!!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	})
+
+	http.ListenAndServe(":80", nil)
 }
